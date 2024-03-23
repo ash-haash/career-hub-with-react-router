2eb976cd88de113a748e4cf37e157703beee2948
ashrafur rahman farhad <itsfaarhaad@gmail.com>
2024-03-23 02:58:24 +0000

    adding data to appliJobs component

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getStoredJobApplication } from "../../utility/localStorage";
 
 const AppliedJobs = () => {
+    const jobs = useLoaderData();
+    useEffect(() => {
+        const storedJobIds = getStoredJobApplication();
+        if (jobs.length > 0) {
+            const jobsApplied = jobs.filter(job => )
+        }
+    }, [])
+
     return (
         <div>
             <h2>Jobs I've Applied</h2>
--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -1,4 +1,7 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { saveJobApplication } from "../../utility/localStorage";
 
 
 const JobDetails = () => {
@@ -7,6 +10,11 @@ const JobDetails = () => {
     const idInt = parseInt(id);
     const job = jobs.find(job => job.id === idInt);
     console.log(job);
+
+    const handleApplyJob = () => {
+        saveJobApplication(id);
+        toast('Application submitted successfully!')
+    }
 
     return (
         <div>
@@ -19,9 +27,10 @@ const JobDetails = () => {
                 </div>
                 <div className="border">
                     <h2 className="text-2xl">Side Things</h2>
-                    <button className="btn btn-primary w-full">Apply Now</button>
+                    <button onClick={handleApplyJob} className="btn btn-primary w-full">Apply Now</button>
                 </div>
             </div>
+            <ToastContainer />
         </div>
     );
 };
